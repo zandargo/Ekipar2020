1329e5bc570fb7f66add7335240d499cfdb56ab8
Madson Germano <madson.br@gmail.com>
2020-09-23 18:18:21 -0300

    WIP

--- js/script.js
+++ js/script.js
@@ -174,6 +174,12 @@ function openTab(evt, tabName) {
 
 	//* Rolar para o tpo da página
 	$('html, body').animate({ scrollTop: 0 }, 'slow')
+
+	//* Redim Img Caroussel
+	$(".flipd").height(300)
+	// // $(".flipd").width('100%')
+	$('.flipd ul').css('margin', '0 20px');
+	// $('.flipd ul').css('margin', '0 auto');
 }
 
 //* ------- INÍCIO DO CARREGAMENTO ------- */
@@ -183,31 +189,6 @@ $('#aempresa').show()
 $('#aempresa, #btn-aempresa').addClass('active')
 $('html, body').animate({ scrollTop: 0 }, 'slow')
 
-// $('.flipster').flipster({
-//     itemContainer: 'ul', //
-//     itemSelector: 'li', //
-//     start: 'center', // ['center'|number]  Zero based index of the starting item, or use 'center' to start in the middle
-//     fadeIn: 400, // [milliseconds] Speed of the fade in animation after items have been setup
-//     loop: true, //[true|false] Loop around when the start or end is reached
-//     autoplay: 3000, // [false|milliseconds] If a positive number, Flipster will automatically advance to next item after that number of milliseconds
-//     pauseOnHover: true, // [true|false] If true, autoplay advancement will pause when Flipster is hovered
-//     style: 'carousel', // [coverflow|carousel|flat|...]
-//     // Adds a class (e.g. flipster--coverflow) to the flipster element to switch between display styles
-//     // Create your own theme in CSS and use this setting to have Flipster add the custom class
-//     spacing: -0.5, // [number] Space between items relative to each item's width. 0 for no spacing, negative values to overlap
-//     click: true, // [true|false] Clicking an item switches to that item
-//     keyboard: true, // [true|false] Enable left/right arrow navigation
-//     scrollwheel: true, // [true|false] Enable mousewheel/trackpad navigation; up/left = previous, down/right = next
-//     touch: true, // [true|false] nable swipe navigation for touch devices
-//     nav: false, // [true|false|'before'|'after'] If not false, Flipster will build an unordered list of the items
-//     // Values true or 'before' will insert the navigation before the items, 'after' will append the navigation after the items
-//     buttons: false, // [true|false|'custom'] If true, Flipster will insert Previous / Next buttons with SVG arrows
-//     // If 'custom', Flipster will not insert the arrows and will instead use the values of `buttonPrev` and `buttonNext`
-//     buttonPrev: '<', // [text|html] Changes the text for the Previous button
-//     buttonNext: '>', // [text|html] Changes the text for the Next button
-//     onItemSwitch: false // [function] Callback function when items are switched
-//     // Arguments received: [currentItem, previousItem]
-// });
 
 
 $(function(){ 
@@ -219,16 +200,6 @@ $(function(){
 		})
 })
 	
-
-// $('.flipd').height(600)
-// $('.flipd').css('height', '400px');
-// $('.flipd > ul').css('height', '400px');
-
-// $(document).ready(function () { 
-// 	$('ul.flip-items').css('height', '400')
-// })
-
-
-$(function() {
-    $('ul.flip-items').css('height', '400')
-});
+// $('.flipd').width('75%')
+// $('.flipd ul').width('75%')
+// $('.flipd ul').width('75%')
